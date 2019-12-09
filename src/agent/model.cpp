047c0920cad8676f72bc6a5fd713289d58cd663a
c16192 <ishidashu@gmail.com>
2019-12-09 18:56:17 +0000

    Added tanh activation

--- src/agent/model.cpp
+++ src/agent/model.cpp
@@ -12,7 +12,8 @@ torch::nn::Sequential Net() {
             torch::nn::Conv2d(torch::nn::Conv2dOptions(8, 8, /*kernel_size=*/5).padding(2).with_bias(false)),
             torch::nn::Functional(torch::leaky_relu, 0.2),
             // Layer 4
-            torch::nn::Conv2d(torch::nn::Conv2dOptions(8, 1, /*kernel_size=*/5).padding(2).with_bias(false))
+            torch::nn::Conv2d(torch::nn::Conv2dOptions(8, 1, /*kernel_size=*/5).padding(2).with_bias(false)),
+            torch::nn::Functional(torch::tanh)
     );
     return model;
 }
